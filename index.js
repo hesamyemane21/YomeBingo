ba608393c38d0a944d3fe84d15fc99e8ea2ef088
hesamyemane21 <hesamyemane21@gmail.com>
2026-08-27 19:37:05 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Telegraf, Markup } = require('telegraf');
+
+const bot = new Telegraf('YOUR_BOT_TOKEN_HERE');
+
+bot.command('start', async (ctx) => {
+  const photoUrl = 'https://hesamyemane21.github.io/YemeBingo/banner.jpg';
+
+  await ctx.replyWithPhoto(photoUrl, {
+    caption: 'Welcome to Yeme Bingo! Play bingo and start winning today!',
+    ...Markup.inlineKeyboard([
+      [Markup.button.webApp('🎮 Play now 🎮', 'https://hesamyemane21.github.io/YemeBingo/')],
+      [
+        Markup.button.callback('Check Balance 💰', 'balance'),
+        Markup.button.callback('Game Instruction 📑', 'instructions')
+      ],
+      [
+        Markup.button.callback('Deposit', 'deposit'),
+        Markup.button.callback('Contact Us 📞', 'contact')
+      ]
+    ])
+  });
+});
+
+bot.action('deposit', (ctx) => ctx.reply('Please enter the amount you want to deposit (50–3000 ETB):'));
+bot.command('deposit', (ctx) => ctx.reply('Please enter the amount you want to deposit (50–3000 ETB):'));
+
+bot.launch();
